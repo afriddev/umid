4f7a35682e3a030b4f73b0bddce315f2d7aa2bf4
afriddev <afridayan01@gmail.com>
2025-01-27 10:26:06 +0530

    added zustand base code

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import Home from "./pages/Home";
 import { useEffect } from "react";
 import { useAppContext } from "./components/appcomponents/AppContext";
 
+import { create } from "zustand";
+
 function App() {
   const { dispatch } = useAppContext();
-
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -15,12 +16,29 @@ function App() {
     });
   }, [pathname]);
 
+  type store = {
+    count: number;
+    inc: () => void;
+  };
+
+  const useCounter = create<store>()((set) => {
+    return {
+      count: 1,
+      inc: () =>
+        set((state) => {
+          return {
+            count: state.count + 1,
+          };
+        }),
+    };
+  });
+
   return (
     <div className="h-full w-full  ">
-      <Routes>
+      {/* <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<Home />} path="/dashboard" />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
